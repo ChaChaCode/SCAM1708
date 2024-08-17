26d47619fc2ddb946a262884894e2f96d7cbdd25
chachacode <103096938+ChaChaCode@users.noreply.github.com>
2024-08-17 22:48:29 +0500

    Update FullOpen.js

--- script/FullOpen.js
+++ script/FullOpen.js
@@ -8,6 +8,8 @@ document.addEventListener('DOMContentLoaded', function() {
     // Разворачиваем WebApp на весь экран
     window.Telegram.WebApp.expand();
 
+    window.Telegram.WebApp.disableVerticalSwipes();
+
     // Устанавливаем цвет фона заголовка
     window.Telegram.WebApp.setHeaderColor('#282d3f');
 
@@ -22,14 +24,4 @@ document.addEventListener('DOMContentLoaded', function() {
         // Адаптируем интерфейс под новый размер
         console.log('Viewport changed:', window.Telegram.WebApp.viewport);
     });
-
-    // Функция для отключения вертикальных свайпов
-    function disableVerticalSwipes() {
-        window.Telegram.WebApp.setSwipeHandling({
-            vertical: false
-        });
-    }
-
-    // Отключаем вертикальные свайпы
-    disableVerticalSwipes();
 });
